798892a839afce247c819773587288bbe8f7970c
Lisander Lopez <lisander@buffalo.edu>
2020-03-10 14:26:15 -0400

    REACT ROUTERWORKING!

---
+++ flipre/project/flip-reset/src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./header/header";
+import Home from "./home/home";
+import DM from "./directmessage/DM";
+import Profile from "./profile/profile";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	useRouteMatch,
+	useParams
+} from "react-router-dom";
+
+function App() {
+	return (
+		<Router>
+			<meta charset="UTF-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<link
+				href="https://fonts.googleapis.com/css?family=Charm:700&display=swap"
+				rel="stylesheet"
+			/>
+			<link
+				rel="stylesheet"
+				href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
+			/>
+			<link rel="stylesheet" href="animate.css" />
+			<Header />
+			<Switch>
+				<Route path="/" exact>
+					<Home />
+				</Route>
+				<Route path="/messages">
+					<DM />
+				</Route>
+				<Route path="/profile">
+					<Profile />
+				</Route>
+			</Switch>
+		</Router>
+	);
+}
+
+export default App;
---
+++ flipre/project/flip-reset/src/profile/profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./profile.css";
+class Profile extends Component {
+	render() {
+		return (
+			<div class="feed">
+				<h1> THIS IS PROFILE</h1>
+			</div>
+		);
+	}
+}
+
+export default Profile;
