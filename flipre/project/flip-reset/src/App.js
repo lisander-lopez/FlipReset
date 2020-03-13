a4fda652f0798a3f5af389a7860c83e82d89d500
Mike Oldman <oldmikeman@gmail.com>
2020-03-13 17:40:01 -0400

    dm page is there...ugly, but it's there

--- flipre/project/flip-reset/src/App.js
+++ flipre/project/flip-reset/src/App.js
@@ -28,6 +28,7 @@ function App() {
 				href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
 			/>
 			<link rel="stylesheet" href="animate.css" />
+			<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"></link>
 			<Header />
 			<Switch>
 				<Route path="/" exact>
