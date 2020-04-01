d1d5f2ce629e83a9320946c7781f5cffba4ea1d6
Mike Oldman <oldmikeman@gmail.com>
2020-04-01 16:03:25 -0400

    working single conversation for DM. Not attached to back end. Will rebase this branch with direct-message, but wanted you all to have my progress thus far.

--- project/flip-reset/src/directmessage/DM.js
+++ project/flip-reset/src/directmessage/DM.js
@@ -1,76 +1,101 @@
 import React, { Component } from "react";
 import "./DM.css";
+
 class DM extends Component {
+
 	constructor(props) {
 		super(props);
 		this.state = {
-			conversee: 'Justin'
+			sender: 'Justin'
 		};
 	}
-	messageTitle(i) {
-		if (i % 2 !== 0) {
-			return (this.state.conversee);
-		}
-		else {
-			return ("Me");
-		}
-	}
 
-	generateMessages() {
-		let messages = [];
-
-		// Outer loop to create parent
-		for (let i = 0; i < 5; i++) {
-			//Create the parent and add the children
-			messages.push(
-				<div class="post animated bounceInLeft delay-1s">
-					<div class="post-header">
-						<div class="post-author">
-							<a href="#">
-								<img
-									src="https://www.speakingtigerbooks.com/wp-content/uploads/2016/09/facebook-default-no-profile-pic.jpg"
-									alt="Author Picture"
-									srcset=""
-								/>
-							</a>
-							<p class="post-name">{this.messageTitle(i)}</p>
-						</div>
-						<div class="post-settings">
-							<a href="#">
-								<img class="pure-img" src="imgs/report.svg" alt="" srcset="" />
-							</a>
-						</div>
-					</div>
-					<Message />
-				</div>
-			);
-		}
-		console.log(messages);
-		return messages;
-	}
 	render() {
 		return (
-			<div class="feed">
-				<ConversationHeader />
-				{this.generateMessages()}
-				<TextBox />
+			<div class="conversation-container">
+				<ConversationContainer />
+			</div>
+		);
+	}
+}
+export default DM;
+
+function newMessage(i, sender, message, didReceive) {
+	console.log(message);
+	return (
+		<Message i={i} sender={sender} message={message} didReceive={didReceive} />
+	);
+}
+
+const TestData = [
+	{ i: 0, sender: "Me", message: "hello there", didReceive: false },
+	{ i: 1, sender: "Justin", message: "General kenobi!", didReceive: true },
+	{ i: 2, sender: "Me", message: "only a sith deals in absolutes!", didReceive: false },
+	{ i: 3, sender: "Justin", message: "take a seat, young skywalker.", didReceive: true },
+	{ i: 4, sender: "Me", message: "I am the senate!", didReceive: false },
+];
+
+class ConversationContainer extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			// messages contains a list of message metadata
+			// the conversation component does all of the rendering
+			messages: [],
+			theirName: 'Justin',
+			messageCount: 0,
+		}
+		this.updateMessageList = this.updateMessageList.bind(this);
+	}
+
+	// will be attached to send button of text box
+	updateMessageList(message) {
+		// const msgs = this.state.messages;
+		// let tempMsgs = [];
+		// tempMsgs.concat(msgs);
+		// tempMsgs.push(message);
+
+		this.setState({
+			messages: this.state.messages.concat(message),
+			messageCount: this.state.messages.length+1
+		});
+	}
+
+	render() {
+		return (
+			<div class="conversation-container">
+				<ConversationHeader
+					sender={this.state.theirName}
+					messageCount={this.state.messageCount}
+				/>
+				<ConversationThread
+					messages={this.state.messages}
+				/>
+				<TextBox
+					i={this.state.messageCount}
+					sender={((this.state.messageCount %2 !== 0) ? this.state.theirName : "Me")}
+					didReceive={((this.state.messageCount %2 !== 0) ? true : false)}
+					udpate={this.updateMessageList}
+				/>
 			</div>
 		);
 	}
 }
 
-export default DM;
-
-class Conversation extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			messages: [],
-		}
-	}
+// Conversation is just a render helper. It is a glorified list, taking in the messages
+// state from conversationContainer
+class ConversationThread extends Component {
 	render() {
 		return (
-			<Message />
+			<ul class="conversation-list">
+				{this.props.messages.map(data => {
+					return (
+						<li key={data.i}>
+							{newMessage(data.i, data.sender, data.message, data.didReceive)}
+						</li>
+					);
+				})}
+			</ul>
 		);
 	}
 }
@@ -78,16 +103,12 @@ class Conversation extends Component {
 class ConversationHeader extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			conversee: 'Justin',
-			messageCount: '5'
-		}
 	}
 	render() {
 		return (
-			<div>
-				<h3>Conversation with {this.state.conversee}</h3>
-				<h5>{this.state.messageCount} messages</h5>
+			<div class="conversation-header">
+				<h3>Conversation with {this.props.sender}</h3>
+				<h5>{this.props.messageCount} messages</h5>
 			</div>
 		);
 	}
@@ -96,15 +117,32 @@ class ConversationHeader extends Component {
 class Message extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			message: 'hi',
-			didReceive: false,
-		}
+		this.toggleReceive = this.toggleReceive.bind(this);
 	}
+
+	// not used yet
+	toggleReceive() {
+		return (this.props.didReceive);
+	}
+
 	render() {
 		return (
-			<div className="message" id={this.state.didReceive}>
-				{this.state.message}
+			<div class="message animated bounceInLeft delay-1s">
+				<div class="message-header">
+					<div class="message-author">
+						<a href="#">
+							<img
+								src="https://www.speakingtigerbooks.com/wp-content/uploads/2016/09/facebook-default-no-profile-pic.jpg"
+								alt="Author Picture"
+								srcset=""
+							/>
+						</a>
+						<p class="message-name">{this.props.sender}</p>
+					</div>
+					<div class="message-text">
+						{this.props.message}
+					</div>
+				</div>
 			</div>
 		);
 	}
@@ -113,28 +151,78 @@ class Message extends Component {
 class TextBox extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			messageText: '',
+			isDisabled: true,
+		};
 	}
+
+	handleChange = name => event => {
+		event.preventDefault();
+		this.setState({
+			[name]: event.target.value,
+			isDisabled: ((this.state.messageText === '') ? true : false)
+		});
+	}
+
+	buildMessageData() {
+		const text = this.state.messageText;
+		console.log("FROM TEXTBOX: " + text);
+		this.setState({
+			messageText: '',
+		});
+		return ([{ i: this.props.i, sender: this.props.sender, message: text, didReceive: this.props.didReceive }]);
+	}
+
 	render() {
 		return (
 			<div class="input-container">
 				<input
 					type="text"
-					name="message"
-					id="postmessage1"
-					class="post-message"
+					name="messageText"
+					id="messageText"
+					class="message-message"
 					placeholder="Enter your message to send..."
-					onKeyUp={this.handleKeyUp}
+					value={this.state.messageText}
+					onChange={this.handleChange("messageText")}
 				/>
-				<a href="#" class="post-message-link disabled">
+				<a
+					href="#"
+					class={`message-message-link ${this.state.isDisabled ? "disabled" : ""}`}
+					onClick={() => this.props.udpate(this.buildMessageData())}
+				>
 					Send
-			</a>
+				</a>
 			</div>
 		);
 	}
 }
 
-class ConversationList extends Component {
+// individual element for AllConversations, clickable card to render the
+// selected conversation
+class ConversationNode extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			lasMessage: 'hi',
+			sender: 'Justin',
+			messageCount: '5'
+		}
+
+	}
+}
+
+// List of all the conversations 
+class AllConversations extends Component {
+	generateConvos() {
+		let convos = [];
+
+	}
+
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		}
 	}
 }
