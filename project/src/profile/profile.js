525a679c6ea06f30c60de3812acf9bfe6e47d630
yes <ok>
2020-04-15 13:15:47 -0400

    please

--- project/src/profile/profile.js
+++ project/src/profile/profile.js
@@ -9,13 +9,13 @@ import {
 	Link,
 	useRouteMatch,
 	withRouter,
-	useParams
+	useParams,
 } from "react-router-dom";
-import { withFirebase } from '../components/Firebase/context'
-import ReactPlayer from 'react-player'
-import { connect } from 'react-redux';
-import { compose } from 'recompose';
-
+import { withFirebase } from "../components/Firebase/context";
+import ReactPlayer from "react-player";
+import { connect } from "react-redux";
+import { compose } from "recompose";
+import { saveURL } from '../redux/actions';
 
 const tit = () => (
 	<div>
@@ -25,56 +25,74 @@ const tit = () => (
 
 
 class Prof extends Component {
-
 	constructor(props) {
 		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
 		this.state = {
-			source: null,
+			source: [],
+			length: null,
 			error: null,
-		}
+		};
 	}
 
-	async componentDidMount() {
-		const response = await this.props.firebase.doGrabFile();
-		this.setState({ source: response });
-		console.log(this.state.source);
+	componentDidMount() {
+		// console.log(this.props.firebase.auth.currentUser.uid);
+		this.props.firebase.getVidList(
+			this.props.firebase.auth.currentUser.uid
+		)
+			.then(result => {
+				this.setState({
+					source: result,
+					length: result.length
+				})
+			},
+				error => {
+					console.log(error)
+				})
 	}
 
+	handleSubmit(e) {
+		console.log(e.target.src);
+		this.props.saveURL(e.target.src)
+		console.log(this.props.user.url)
+	}
 
 	generatePosts() {
 		let posts = [];
-		for (let i = 0; i < 20; i++) {
+		let views = this.state.source
+		for (let i = 0; i < this.state.length; i++) {
+			console.log(views[i])
 			posts.push(
-				<Link to="/chatpro" className="Deeznutsidk">
+				<Link to="/chatpro" className="Deeznutsidk" onClick={this.handleSubmit}>
 					<div class="profile-post-content">
-						<div className='player-wrapper'>
+						<div className="player-wrapper">
 							<ReactPlayer
-								className='react-player'
-								url={this.state.source}
-								width='100%'
-								height='100%'
+								className="react-player"
+								url={views[i]}
+								width="100%"
+								height="100%"
 							/>
 						</div>
 					</div>
 				</Link>
 			);
 		}
-		console.log(posts);
 		return posts;
 	}
 	render() {
-		return ([
+		return [
 			<div class="grid-container">
-				<div class="item1">USERNAME</div>
-				<div class="item2"><img src={lessangry}></img></div>
+				<div class="item1">{this.props.user.username}</div>
+				<div class="item2">
+					<img src={lessangry}></img>
+				</div>
 				<div class="item3">FRIENDS: 23</div>
-				<div class="item4">POSTS: 20</div>
+				<div class="item4">POSTS: {this.state.length}</div>
 				<div class="item5">MESSAGE</div>
 				<div class="item6">FRIEND REQUEST</div>
 			</div>,
-			<div class="profile-feed">{this.generatePosts()}</div>
-		]);
-
+			<div class="profile-feed">{this.generatePosts()}</div>,
+		];
 	}
 }
 
@@ -87,10 +105,11 @@ const mapStateToProps = (state) => {
 	const { user } = state;
 	return {
 		user,
-	}
+	};
+};
+
+const mapDispatchToProps = {
+	saveURL,
 }
 
-
-export default connect(mapStateToProps, null)(Profile);
-
-
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
